ae505a085351d47f7a49f7116923033f677b8876
Shubham Sharma <shubhamku044@gmail.com>
2022-10-16 19:17:24 +0530

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,64 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Lottie, { LottieRefCurrentProps } from 'lottie-react'
+import React, { useRef, useState } from 'react'
+import lottieAnimation from '../public/lottie-animation.json'
+
+const styles = {
+	button: 'text-white font-bold cursor-pointer py-2 px-4 rounded',
+}
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const [direction, setDirection] = useState<1 | -1>(1)
+	const [speed, setSpeed] = useState<number>(1)
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+	const lottieRef = useRef<LottieRefCurrentProps | null>(null)
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+	return (
+		<div className="min-h-screen flex flex-col space-y-4 justify-center items-center">
+			<Head>
+				<title>Lottie Animation</title>
+			</Head>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
+			<div className="h-[32rem] w-[32rem] bg-yellow-300/10">
+				<Lottie lottieRef={lottieRef} animationData={lottieAnimation} />
+			</div>
+			<button
+				onClick={() => {
+					lottieRef.current?.play()
+				}}
+				className={`${styles.button} bg-blue-500 hover:bg-blue-700`}
+			>
+				Play
+			</button>
+			<button
+				onClick={() => {
+					lottieRef.current?.pause()
+				}}
+				className={`${styles.button} bg-red-500 hover:bg-red-700`}
+			>
+				Pause
+			</button>
+			<button
+				onClick={() => {
+					setDirection(direction === 1 ? -1 : 1)
+					lottieRef.current?.setDirection(direction)
+				}}
+				className={`${styles.button} bg-yellow-500 hover:bg-yellow-700`}
+			>
+				Update direction
+			</button>
+			<button
+				onClick={() => {
+					setSpeed((speed) => (speed < 2 ? speed + 0.25 : 1))
+					lottieRef.current?.setSpeed(speed)
+				}}
+				className={`${styles.button} bg-green-500 hover:bg-green-700`}
+			>
+				Update speed
+			</button>
+		</div>
+	)
 }
 
 export default Home
